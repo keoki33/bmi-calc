745dae0ad279be23a4be1029d7b6f8d42155369c
keoki33 <gwong323@yahoo.com>
2020-03-27 19:13:16 +1100

    1st challenge complete

--- BMI-Calculator-LayoutPractice/Controllers/ViewController.swift
+++ BMI-Calculator-LayoutPractice/Controllers/ViewController.swift
@@ -9,12 +9,31 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var heightSliderLabel: UILabel!
+    
+    @IBOutlet weak var weightSliderLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+    
+    @IBAction func heightSliderChange(_ sender: UISlider) {
+        heightSliderLabel.text = String(format: "%.1fm", sender.value)
+    }
+    
+    
+    
+    @IBAction func weightSliderChange(_ sender: UISlider) {
+        weightSliderLabel.text = "\(String(Int(sender.value)))Kg"
+    }
+    
+    
+    func udateUi()  {
+        
+    }
 
 }
 
